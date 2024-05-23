dbf74bd9a3d7170821f15d39a87b964b46cc5d59
vfudtra <vfdutra00@gmail.com>
2024-05-22 23:38:12 -0300

    fix: remove CORS Origins restriction

--- public/index.php
+++ public/index.php
@@ -35,7 +35,7 @@ if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
 }
 
 // CORS CONFIGURATION
-header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 $method = $_SERVER['REQUEST_METHOD'];
